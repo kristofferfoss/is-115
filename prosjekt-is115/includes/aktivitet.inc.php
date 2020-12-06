503eb68eb80d93af034f57acb86c47ed35cd8ee2
Ole Kristoffer Foss <olekf17@uia.no>
2020-12-06 15:26:53 +0100

    Ferdig aktivitet uten kvalitetssikring

--- prosjekt-is115/includes/aktivitet.inc.php
+++ prosjekt-is115/includes/aktivitet.inc.php
@@ -22,21 +22,23 @@
             echo "<tr><td><strong> Dato: </strong><br>" . $row['activityDate'] . "</td><td><strong> Aktivitet: </strong><br>" . $row['activityDesc'] . "</td><td><strong> Ansvarlig: </strong><br>" . $row['activityAnsvarlig'] . "</td><td><strong> Starter: </strong><br>" . $row['activityStarttid'] . "</td><td><strong> Slutter: </strong><br>" . $row['activitySlutttid'] . "</td><td><strong> Sted: </strong><br>" . $row['activitySted'] . "</td><td><form method='post'> <button type='submit' name='book' value='$row[activityId]'> Meld deg på </button> </form></td></tr>";  
         }
         echo "</table";
-        mysqli_close($conn); 
+        //mysqli_close($conn); 
     }
 
     function bookActivity($conn, $actID) 
     {
         $userID = $_SESSION["userid"];
         
-        $sql = "INSERT INTO booking(booking_userId, booking_activityId) + VALUES(?, ?)";
+        $sql = "INSERT INTO booking (booking_userId, booking_activityId) VALUES(?, ?)";
 
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         
         $stmt = mysqli_stmt_init($conn);
 
         mysqli_stmt_prepare($stmt, $sql);
+
         mysqli_stmt_bind_param($stmt, "ii", $userID, $actID);
+
         mysqli_stmt_execute($stmt);
 
         mysqli_close($conn);
